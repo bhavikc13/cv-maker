612987d3a81be5dad9276ce1df7ceeabedee34b2
bhavikc13 <62197899+bhavikc13@users.noreply.github.com>
2020-03-16 16:14:24 +0530

    Delete CvPreview.js

--- src/components/cvs/CvPreview.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const CvPreview = props => {
-  const id = props.match.params.id;
-  return <div className="container">{id} - Bhavik Chaudhary</div>;
-};
-
-export default CvPreview;
